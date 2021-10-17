2e1fa72788a09373acbe8a5f52248824d420a67b
Deven-14 <deven.paramaj@gmail.com>
2021-10-18 03:23:56 +0530

    integration of front and back end

---
+++ data_collectors/get_achievements.js
@@ -0,0 +1,158 @@
+const {google} = require('googleapis');
+const auth = require('../auth/get_auth');
+const get_spreadsheetId = require('./get_spreadsheetId');
+
+function add_sheet_to_get_achievements(sheets, spreadsheetId, userData)
+{
+    return new Promise((resolve, reject) => {
+
+        var requests = [];
+
+        requests.push({
+            addSheet: { 
+                properties:{
+                    sheetId: userData.usn,
+                    title: userData.email
+                }
+            },
+        });
+
+        const batchUpdateRequest = {requests};
+
+        sheets.spreadsheets.batchUpdate({
+            spreadsheetId,
+            resource: batchUpdateRequest,
+        }, (err, response) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("sheet added");
+                resolve("added Sheet to_get_achievements");
+            }
+        });
+    });
+}
+
+function delete_sheet_of_achievemets(sheets, spreadsheetId, userData)
+{
+    return new Promise((resolve, reject) => {
+
+        var requests = [];
+
+        requests.push({
+            deleteSheet: {
+                sheetId: userData.usn
+            },
+        })
+
+        const batchUpdateRequest = {requests};
+
+        sheets.spreadsheets.batchUpdate({
+            spreadsheetId,
+            resource: batchUpdateRequest,
+        }, (err, response) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("deleted sheet");
+                resolve("delete sheet of achievements");
+            }
+        });
+    });
+}
+
+
+function get_achievement_of_year_by_filter(sheets, spreadsheetId, userData) {
+
+    return new Promise((resolve, reject) => {
+
+        let values = [
+            [
+                `=FILTER(year1!A:G, year1!A:A=${userData.usn})`, null, null, null, null, null, null,
+                `=FILTER(year2!A:G, year2!A:A=${userData.usn})`, null, null, null, null, null, null,
+                `=FILTER(year3!A:G, year3!A:A=${userData.usn})`, null, null, null, null, null, null,
+                `=FILTER(year4!A:G, year4!A:A=${userData.usn})`
+            ]
+        ];
+
+        var range = `${userData.email}!A:G`;
+        const resource = {
+            values,
+        };
+        var valueInputOption = "USER_ENTERED";
+        
+        sheets.spreadsheets.values.append({
+            spreadsheetId,
+            range,
+            valueInputOption,
+            resource,
+        }, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("year filter added");
+                resolve("year filter added");
+            }
+        });
+
+    });
+}
+
+function get_achievements(sheets, spreadsheetId, userData)
+{
+    return new Promise((resolve, reject) => {
+        var ranges = [`${userData.email}!A:G`,`${userData.email}!H:N`,`${userData.email}!O:U`,`${userData.email}!V:AB`];
+        sheets.spreadsheets.values.batchGet({
+            spreadsheetId,
+            ranges,
+        }, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("got all achievements");
+                data = {};
+                for(let i = 0; i < 4; ++i)
+                    data[`year${i+1}`] = result.data.valueRanges[i].values;
+                resolve(data);
+            }
+        });
+    });
+}
+
+// var userData = {
+//     usn: 48,
+//     name: "Deven Prakash Paramaj",
+//     email: "devenparamaj.is19@bmsce.ac.in",
+//     phone: "1234567890",
+//     nameOfEvent: "ab",
+//     detailsOfEvent: "cd",
+//     level: "ef",//"Level(state/national/international)",
+//     award: "fg",
+//     department_id: "1s0xIu0UaUhwwRVsOybSz4CyM514u9sA0",
+//     batch: 2018,
+//     year: 1,
+// };
+
+
+
+
+
+module.exports = (userData) => {
+//async function main() {
+    
+    return new Promise(async (resolve, reject) => {
+
+        const sheets = google.sheets({version: 'v4', auth});
+        const spreadsheetId = await get_spreadsheetId(auth, userData.department_id, userData.batch);
+        await add_sheet_to_get_achievements(sheets, spreadsheetId, userData);
+        await get_achievement_of_year_by_filter(sheets, spreadsheetId, userData);
+        const data = await get_achievements(sheets, spreadsheetId, userData);
+        await delete_sheet_of_achievemets(sheets, spreadsheetId, userData);
+        //console.log(data, "final data");
+        resolve(data);
+        
+    });
+
+}
+
+//main()
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-// const add_data = require('./data_collectors/add_data');
-// const get_user_data = require('./data_collectors/get_user_data');
-// const validate_ph_number = require('./data_collectors/validate_ph_number');
+const add_data = require('./data_collectors/add_data');
+const get_user_data = require('./data_collectors/get_user_data');
+const validate_ph_number = require('./data_collectors/validate_ph_number');
+const get_achievements = require('./data_collectors/get_achievements');
 
 
 const port = 3000;
@@ -25,14 +25,12 @@ var userData = {
     year: null
 };
 
+var USN;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
-
-// app.get("/", (req, res) => {
-//     res.render("index.ejs");
-// });
 
 //Static Files
 app.use(express.static('public'));
@@ -46,21 +44,21 @@ app.get("/", (req, res) => {
 
 app.post("/addAchievement", async (req, res) => {
     try{
-        // //validate email also
-         userData.name = req.body.name;
-         userData.email = req.body.email;
-         userData.image = req.body.image;
-         if( !userData.name || !userData.image || !userData.email)
+        userData.name = req.body.name;
+        userData.email = req.body.email;
+        userData.image = req.body.image;
+
+        if( !userData.name || !userData.image || !userData.email)
             throw new Error("Invalid Not logged in");
-            
-        // console.log(userData);
-        // data = await get_user_data(req.body.usn);
-        // userData.usn = data.usn;
-        // userData.department_id = data.department_id;
-        // userData.batch = data.batch;
-        // await validate_ph_number(req.body.phone_no);
-        // userData.phone = req.body.phone_no;
-         console.log(userData);
+        USN = req.body.usn;
+        data = await get_user_data(req.body.usn);
+        userData.usn = data.usn;
+        userData.department_id = data.department_id;
+        userData.batch = data.batch;
+        console.log(req.body.phone_no);
+        await validate_ph_number(req.body.phone_no);
+        userData.phone = req.body.phone_no;
+        console.log(userData);
         res.render("addAchievement.ejs", {isValid: null,image:userData.image,name:userData.name});
     }catch(error){
         res.render("usn.ejs", {isValid: false})
@@ -74,19 +72,21 @@ app.post("/addAchievement_again", async (req, res) => {
         userData.award = req.body.award;
         userData.level = req.body.level;
         userData.year = parseInt(req.body.year);
-        // for(let field in userData)
-        //     if(!userData[field])
-        //         throw new Error("Invalid");
-        // //await add_data(userData);
-         console.log(userData);
+        console.log(userData);
+        for(let field in userData)
+            if(field != 'year1' && field != 'year2' && field != 'year3' && field != 'year4' && !userData[field])
+                throw new Error("Invalid");
+        await add_data(userData);
+        console.log(userData);
         res.render("addAchievement.ejs", {isValid: true,image:userData.image,name:userData.name});
     }catch(error){
         res.render("addAchievement.ejs", {isValid: false,image:userData.image,name:userData.name});
     }
 });
 
-app.get("/viewAchievements", (req, res) => {
-    res.render("viewAchievements.ejs", {isValid: true,image:userData.image,name:userData.name});
+app.get("/viewAchievements",async (req, res) => {
+    const data = await get_achievements(userData);
+    res.render("viewAchievements.ejs", {isValid: true, userData: userData, usn: USN, achievements: data});
 });
 
 app.listen(port,() => {
