432760858ae7955a12ff11955094535ed0678fc3
Deven-14 <deven.paramaj@gmail.com>
2021-11-10 22:25:08 +0530

    added upload file and changed backend to work with it

---
+++ functions/add_file_to_temp.js
@@ -0,0 +1,19 @@
+function add_file_to_temp(file, filename)
+{
+    return new Promise((resolve, reject) => {
+        
+        var filepath = `./temp/${filename}`; // only allow to upload pdf
+        file.mv(filepath, (err) => {
+            if(err) {
+                console.log("couldn't upload to node js server", err);
+                reject("upload failed to node js server");
+            } else {
+                console.log("file uploaded to node js server");
+                resolve(filepath);
+            }
+        });
+
+    });
+}
+
+module.exports = add_file_to_temp;
---
+++ functions/upload_certificate.js
@@ -0,0 +1,75 @@
+const {google} = require('googleapis');
+const protected_data = require('../auth/protected_Data.json');
+const fs = require('fs');
+
+
+function get_certificate_folder_id(auth, spreadsheetId) {
+    
+    return new Promise((resolve, reject) => {
+
+        var request = {
+            dataFilters: [
+                {
+                    developerMetadataLookup: {
+                        metadataKey: "Certificates",
+                        visibility: "PROJECT"
+                    }
+                }
+            ]
+        };
+
+        const resource = request;
+
+        const sheets = google.sheets({ version: 'v4', auth });
+
+        sheets.spreadsheets.developerMetadata.search({
+            spreadsheetId,
+            resource
+        }, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                var folder_id = result.data.matchedDeveloperMetadata[0].developerMetadata.metadataValue;
+                console.log("certificate folder id", folder_id);
+                resolve(folder_id);
+            }
+        });
+    });
+}
+
+function upload_certificate(auth, filepath, filename)
+{
+    return new Promise(async (resolve, reject) => {
+
+        var folder_id = await get_certificate_folder_id(auth, protected_data.index_table_id);
+
+        var fileMetadata = {
+            name: `${filename}.pdf`,
+            parents: [folder_id]
+        };
+
+        var media = {
+            mimeType: 'application/pdf',
+            body: fs.createReadStream(filepath)
+        };
+
+        const drive = google.drive({version: 'v3', auth});
+
+        drive.files.create({
+            resource: fileMetadata,
+            media: media,
+            fields: "id, webViewLink",
+            uploadType: 'media'
+        }, function (err, file) {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log("file uploaded to drive successfully", file.data.webViewLink);
+                resolve(file.data.webViewLink);
+            }
+        });
+    });
+}
+
+module.exports = upload_certificate;
